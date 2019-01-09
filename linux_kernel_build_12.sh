dc3d035749d9a29d5a9ab64ac208a35abe1e431c
ARJUN <arjuncr00@gmail.com>
2019-01-09 22:35:45 +0530

    fix for alignment

--- linux_kernel_build_12.sh
+++ linux_kernel_build_12.sh
@@ -18,7 +18,6 @@ make ARCH=${OS_ARCH} \
 CROSS_COMPILE=${OS_TARGET}- \
 INSTALL_MOD_PATH=${OS} modules_install
 
-
 cp -v arch/x86/boot/bzImage ${OS}/boot/vmlinuz-4.16.3
 cp -v System.map ${OS}/boot/System.map-4.16.3
 cp -v .config ${OS}/boot/config-4.16.3
