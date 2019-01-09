d8cf8a2739f21b41ac2e14f24593fdb198c67912
ARJUN <arjuncr00@gmail.com>
2019-01-09 22:30:02 +0530

    fix for alignment

--- build_zlib_15.sh
+++ build_zlib_15.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 . ./build_env_0.sh
 
 echo "building zlib"
@@ -15,7 +14,6 @@ mv -v ${OS}/usr/lib/libz.so.* ${OS}/lib
 ln -svf ../../lib/libz.so.1 ${OS}/usr/lib/libz.so
 ln -svf ../../lib/libz.so.1 ${OS}/usr/lib/libz.so.1
 ln -svf ../lib/libz.so.1 ${OS}/lib64/libz.so.1
-
 
 echo "completed zlib build"
 
