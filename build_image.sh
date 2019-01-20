613697ad0512775facd74e9b9028f9faf89452f3
arjuncr <>
2019-01-20 15:36:11 +0530

    adding build script

---
+++ build_image.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+. ./build_env_0.sh
+
+. ./create_target_filesystem_1.sh
+
+. ./dir_for_cross_cc_2.sh
+
+. ./basic_system_file_3.sh
+
+. ./cross_compile_env_4.sh
+
+. ./linux_headers_5.sh
+
+. ./binutil_build_6.sh
+
+. ./gcc_build_7.sh
+
+. ./build_glibc_8.sh
+
+. ./gcc_final_buid_10.sh
+
+. ./build_busy_box_11.sh
+
+. ./linux_kernel_build_12.sh
+
+. ./build_boot_14.sh
+
+. ./build_zlib_15.sh
+
+. ./install_target_image_16.sh
+
+echo "build completed"
+
