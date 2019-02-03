38c6c460a7c396397b3e86172988487c6064b22b
ARJUN <arjuncr00@gmail.com>
2019-02-03 23:11:30 +0530

    Update binutil_build_6.sh

--- binutil_build_6.sh
+++ binutil_build_6.sh
@@ -6,11 +6,11 @@ mkdir work/binutils-build
 
 cd work/binutils-build/
 
-../../source/binutil/configure --prefix=${OS}/cross-tools \
+../../source/binutil/configure --prefix=${CROSS_CC}/cross-tools \
 --target=${OS_TARGET} --with-sysroot=${OS} \
 --disable-nls --enable-shared --disable-multilib
 make configure-host && make
-ln -sv lib ${OS}/cross-tools/lib64
+ln -sv lib ${CROSS_CC}/cross-tools/lib64
 make install
 
 cp -v ../source/binutil/include/libiberty.h ${OS}/usr/include
